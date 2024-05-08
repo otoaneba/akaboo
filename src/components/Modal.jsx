cbc80a187671193c94d337cb40967f3904d8eee7
Naoto Abe <naotoabe1@gmail.com>
2024-05-08 16:56:36 +0200

    style modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -60,7 +60,7 @@ export default function Modal({ isOpen, closeModal, handleSubmit, handleChange }
                             Both
                         </label>
                     </fieldset>
-                    <button type="submit">Secure My Spot!</button>
+                    <button className="submit-button" type="submit">Secure My Spot!</button>
                 </form>
                 <button onClick={closeModal} className="close-button">&times;</button>
             </div>
