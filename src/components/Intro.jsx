0daa00abf8894714ba1b1046743d0a186f2983a8
Cynthia Mengyuan Li <cynthiamengyuanli@gmail.com>
2024-05-13 10:03:01 +0200

    clean up

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import { addDoc, getDocs } from "firebase/firestore";
 import { userCollection, app } from "../firebase";
-import {
-	getAuth,
-	sendEmailVerification,
-	onAuthStateChanged,
-} from "firebase/auth";
 import "firebaseui/dist/firebaseui.css";
-import firebase from "firebase/compat/app";
-import * as firebaseui from "firebaseui";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import Modal from "./Modal";
-import modalData from "../assets/modalData";
 
 export default function Intro() {
-	const [userEmail, setUserEmail] = React.useState("");
-
-	const functions = getFunctions(app);
-	const sendEmailFunction = httpsCallable(functions, "sendEmail");
 	const [modalOpen, setModalOpen] = React.useState(false);
 	const [formData, setFormData] = React.useState({
 		name: "",
@@ -49,33 +37,32 @@ export default function Intro() {
 		}
 	};
 
-  const handleSubmit = (e) => {
-      e.preventDefault();
-      setModalOpen(false);
-      console.log('form submit')
-      submitForm();
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setModalOpen(false);
+		console.log("form submit");
+		submitForm();
+	};
 
-  const closeModal = () => {
-      setActiveModal(-1);
-  };
+	const closeModal = () => {
+		setActiveModal(-1);
+	};
 
-  async function submitForm() {
-    const newNote = formData;
-    setFormData({...formData, createdAt: Date.now()})
-    const newNoteRef = await addDoc(userCollection, newNote)
-    setFormData({
-      name: '',
-      email: '',
-      zipCode: '',
-      ageRange: '',
-      interest: [],
-      categories: [],
-      createdAt: ''
-  })
-  console.log(newNoteRef)
-}
-
+	async function submitForm() {
+		const newNote = formData;
+		setFormData({ ...formData, createdAt: Date.now() });
+		const newNoteRef = await addDoc(userCollection, newNote);
+		setFormData({
+			name: "",
+			email: "",
+			zipCode: "",
+			ageRange: "",
+			interest: [],
+			categories: [],
+			createdAt: "",
+		});
+		console.log(newNoteRef);
+	}
 
 	return (
 		<div className="intro">
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,68 +1,72 @@
-import React from 'react'
+import React from "react";
+import {
+	getAuth,
+	sendEmailVerification,
+	onAuthStateChanged,
+} from "firebase/auth";
+import firebase from "firebase/compat/app";
+import * as firebaseui from "firebaseui";
 
 export default function SignUp() {
-      // React.useEffect(() => {
-      // 	const ui =
-      // 		firebaseui.auth.AuthUI.getInstance() ||
-      // 		new firebaseui.auth.AuthUI(getAuth(app));
-      // 	const auth = getAuth();
-      // 	onAuthStateChanged(auth, (user) => {
-      // 		if (user) {
-      // 			// User is signed in, see docs for a list of available properties
-      // 			// https://firebase.google.com/docs/reference/js/auth.user
-      // 			const uid = user.uid;
-      // 			console.log("uid", uid);
-      // 		} else {
-      // 			// User is signed out
-      // 			console.log("user is signed out");
-      // 		}
-      // 	});
-      // 	const uiConfig = {
-      // 		signInSuccessUrl: "/",
-      // 		signInOptions: [
-      // 			{
-      // 				provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      // 				clientId:
-      // 					"941019739189-846mrcmc3kffdcmo51b9ppj51tmg13j0.apps.googleusercontent.com",
-      // 				fullLabel: "Join With Google",
-      // 			},
-      // 			{
-      // 				provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      // 				fullLabel: "Join With Email",
-      // 			},
-      // 		],
-      // 		credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
-      // 		callbacks: {
-      // 			signInSuccessWithAuthResult: function (
-      // 				authResult,
-      // 				redirectUrl
-      // 			) {
-      // 				if (
-      // 					authResult.user &&
-      // 					authResult.additionalUserInfo.isNewUser
-      // 				) {
-      // 					console.log(
-      // 						"authResult.user",
-      // 						authResult,
-      // 						authResult.user
-      // 					);
-      // 					// This is a new user, check if email is verified
-      // 					if (!authResult.user.emailVerified) {
-      // 						sendEmailVerification(authResult.user);
-      // 					}
-      // 				}
-      // 				// Return true to redirect to the signInSuccessUrl
-      // 				return true;
-      // 			},
-      // 		},
-      // 	};
+	React.useEffect(() => {
+		const ui =
+			firebaseui.auth.AuthUI.getInstance() ||
+			new firebaseui.auth.AuthUI(getAuth(app));
+		const auth = getAuth();
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// User is signed in, see docs for a list of available properties
+				// https://firebase.google.com/docs/reference/js/auth.user
+				const uid = user.uid;
+				console.log("uid", uid);
+			} else {
+				// User is signed out
+				console.log("user is signed out");
+			}
+		});
+		const uiConfig = {
+			signInSuccessUrl: "/",
+			signInOptions: [
+				{
+					provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+					clientId:
+						"941019739189-846mrcmc3kffdcmo51b9ppj51tmg13j0.apps.googleusercontent.com",
+					fullLabel: "Join With Google",
+				},
+				{
+					provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+					fullLabel: "Join With Email",
+				},
+			],
+			credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
+			callbacks: {
+				signInSuccessWithAuthResult: function (
+					authResult,
+					redirectUrl
+				) {
+					if (
+						authResult.user &&
+						authResult.additionalUserInfo.isNewUser
+					) {
+						console.log(
+							"authResult.user",
+							authResult,
+							authResult.user
+						);
+						// This is a new user, check if email is verified
+						if (!authResult.user.emailVerified) {
+							sendEmailVerification(authResult.user);
+						}
+					}
+					// Return true to redirect to the signInSuccessUrl
+					return true;
+				},
+			},
+		};
 
-      // 	ui.start("#firebaseui-auth-container", uiConfig);
-      // }, []);
-  return (
-				{/* <div
-					className="signUp--buttons"
-					id="firebaseui-auth-container"
-				></div> */}
-  )
+		ui.start("#firebaseui-auth-container", uiConfig);
+	}, []);
+	return (
+		<div className="signUp--buttons" id="firebaseui-auth-container"></div>
+	);
 }
