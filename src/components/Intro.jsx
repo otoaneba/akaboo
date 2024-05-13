df727ab2fd4abc6eb8eadd397e84a107b9409578
Naoto Abe <naotoabe1@gmail.com>
2024-05-13 09:05:38 +0200

    add date field for form and remove signup

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -28,65 +28,6 @@ export default function Intro() {
 		categories: [],
 	});
 
-	React.useEffect(() => {
-		const ui =
-			firebaseui.auth.AuthUI.getInstance() ||
-			new firebaseui.auth.AuthUI(getAuth(app));
-		const auth = getAuth();
-		onAuthStateChanged(auth, (user) => {
-			if (user) {
-				// User is signed in, see docs for a list of available properties
-				// https://firebase.google.com/docs/reference/js/auth.user
-				const uid = user.uid;
-				console.log("uid", uid);
-			} else {
-				// User is signed out
-				console.log("user is signed out");
-			}
-		});
-		const uiConfig = {
-			signInSuccessUrl: "/",
-			signInOptions: [
-				{
-					provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-					clientId:
-						"941019739189-846mrcmc3kffdcmo51b9ppj51tmg13j0.apps.googleusercontent.com",
-					fullLabel: "Join With Google",
-				},
-				{
-					provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-					fullLabel: "Join With Email",
-				},
-			],
-			credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
-			callbacks: {
-				signInSuccessWithAuthResult: function (
-					authResult,
-					redirectUrl
-				) {
-					if (
-						authResult.user &&
-						authResult.additionalUserInfo.isNewUser
-					) {
-						console.log(
-							"authResult.user",
-							authResult,
-							authResult.user
-						);
-						// This is a new user, check if email is verified
-						if (!authResult.user.emailVerified) {
-							sendEmailVerification(authResult.user);
-						}
-					}
-					// Return true to redirect to the signInSuccessUrl
-					return true;
-				},
-			},
-		};
-
-		ui.start("#firebaseui-auth-container", uiConfig);
-	}, []);
-
 	const handleChange = (e) => {
 		const { name, value, type, checked } = e.target;
 		if (type === "checkbox") {
@@ -122,6 +63,7 @@ export default function Intro() {
 
   async function submitForm() {
     const newNote = formData;
+    setFormData({...formData, createdAt: Date.now()})
     const newNoteRef = await addDoc(userCollection, newNote)
     setFormData({
       name: '',
@@ -129,7 +71,8 @@ export default function Intro() {
       zipCode: '',
       ageRange: '',
       interest: [],
-      categories: []
+      categories: [],
+      createdAt: ''
   })
 }
 
@@ -156,10 +99,6 @@ export default function Intro() {
 					handleSubmit={handleSubmit}
 					handleChange={handleChange}
 				/>
-				<div
-					className="signUp--buttons"
-					id="firebaseui-auth-container"
-				></div>
 			</div>
 		</div>
 	);
